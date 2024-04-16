dbb67433062dbe46d2a1e964af9a5afb05976f39
Yue-Heng <128766260+Yue-Heng@users.noreply.github.com>
2024-04-16 10:16:20 +0800

    add img

---
+++ LeeYueHeng/customer/Login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login/Sign up</title>
+    <link rel="stylesheet" type="text/css" href="maindes.css"/>
+</head>
+<body>
+    <div id="header">
+        <h3>
+            <p>Login/SIgn up</p>
+        </h3>
+    </div>
+    <a href="../main.php">Login</a>
+</body>
+</html>
--- LeeYueHeng/main(notlogin).php
+++ LeeYueHeng/main(notlogin).php
@@ -12,14 +12,9 @@
             <a href="Nowshowing.php" class="left-links">Now Showing</a>
             <a href="Upcoming.php" class="left-links">Upcoming</a>
             <a href="Comingsoon.php" class="left-links">Coming Soon</a>
-            <a href="customer/Customer.php" class="right-links">Profile</a>
+            <a href="customer/Login.php" class="right-links">Login/Sign up</a>
         </h3>
     </div>
-    <div id="movie">
-        <img src="movie1.jpg">
-        <img src="movie2.jpg">
-        <img src="movie3.jpg">
-    </div>
-
+    
 </body>
 </html>
