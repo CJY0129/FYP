1e3d4178b3b45af20fa339c0bc7830d3ff40735a
ChengJingYi-1211208060 <128442195+ChengJingYi-1211208060@users.noreply.github.com>
2024-04-20 04:00:01 +0800

    UPGRADE SHOWTIME FIN M3

--- ChengJingYi/admin/delete_showtime.php
+++ ChengJingYi/admin/delete_showtime.php
@@ -3,18 +3,19 @@ if(isset($_GET['show_id'])) {
     include("connect.php");
 
     $show_id = $_GET['show_id'];
-    $sql = "DELETE FROM showtime WHERE show_id = $show_id";
 
-    if(mysqli_query($conn, $sql)) {
+    // Check if the user confirmed the deletion
+  
+        $sql = "DELETE FROM showtime WHERE show_id = $show_id";
 
-        header("location:home.php?show=showtime&success=2");
-        exit();
-    } else {
-
-        header("location:home.php?show=showtime&error=2");
-        exit();
-    }
-
+        if(mysqli_query($conn, $sql)) {
+            header("location:home.php?show=showtime&success=2");
+            exit();
+        } else {
+            header("location:home.php?show=showtime&error=2");
+            exit();
+        }
+    
 
     mysqli_close($conn);
 } 
