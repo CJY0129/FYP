53657dbfa97b72ac6f4fedfa57b9530c7b9c5c1e
ChengJingYi-1211208060 <128442195+ChengJingYi-1211208060@users.noreply.github.com>
2024-05-14 11:52:32 +0800

    update admin

--- ChengJingYi/admin/home.php
+++ ChengJingYi/admin/home.php
@@ -85,7 +85,7 @@ if (!isset($_SESSION['admin_id'])) {
       <nav id="sidebar">
         <!-- Sidebar Header-->
         <div class="sidebar-header d-flex align-items-center">
-          <div class="avatar"><img src="img/avatar-6.jpg" alt="..." class="img-fluid rounded-circle"></div>
+          <div class="avatar"><img src="img/user.png" alt="..." class="img-fluid rounded-circle"></div>
             <div class="title">
             <h1 class="h5">
               <?php
--- ChengJingYi/admin/register.php
+++ ChengJingYi/admin/register.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Dark Bootstrap Admin </title>
+    <title>CineTime new Admin </title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="all,follow">
@@ -20,69 +20,120 @@
     <!-- Custom stylesheet - for your changes-->
     <link rel="stylesheet" href="css/custom.css">
     <!-- Favicon-->
-    <link rel="shortcut icon" href="img/favicon.ico">
+    <link rel="shortcut icon" href="img/CT.ico">
     <!-- Tweaks for older IEs--><!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
-  </head>
-  <body>
+</head>
+<body>
     <div class="login-page">
-      <div class="container d-flex align-items-center">
-        <div class="form-holder has-shadow">
-          <div class="row">
-            <!-- Logo & Information Panel-->
-            <div class="col-lg-6">
-              <div class="info d-flex align-items-center">
-                <div class="content">
-                  <div class="logo">
-                    <h1>Dashboard</h1>
-                  </div>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <div class="container d-flex align-items-center">
+            <div class="form-holder has-shadow">
+                <div class="row">
+                    <!-- Logo & Information Panel-->
+                    <div class="col-lg-6">
+                        <div class="info d-flex align-items-center">
+                            <div class="content">
+                                <div class="logo">
+                                    <h1>CineTime</h1>
+                                </div>
+                                <p>Add new admin</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Form Panel -->
+                    <div class="col-lg-6 bg-white">
+                        <div class="form d-flex align-items-center">
+                            <div class="content">
+                                <?php
+                                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+                                    include("connect.php");
+
+                                    if ($conn->connect_error) {
+                                        die("Connection failed: " . $conn->connect_error);
+                                    }
+
+                                    // Get form data
+                                    $admin_username = $_POST['registerUsername'];
+                                    $admin_email = $_POST['registerEmail'];
+                                    $admin_password = $_POST['registerPassword'];
+                                    $admin_name = $_POST['registerName'];
+                                    $admin_phone = $_POST['registerPhone'];
+                                    $is_admin = isset($_POST['isAdmin']) ? 1 : 0;
+                                    $is_super_admin = isset($_POST['isSuperAdmin']) ? 1 : 0;
+
+                                    // Check if username or email already exists
+                                    $checkUserSql = "SELECT * FROM admin WHERE username='$admin_username' OR Email='$admin_email'";
+                                    $result = $conn->query($checkUserSql);
+
+                                    if ($result->num_rows > 0) {
+                                        echo "<div class='alert alert-danger'>Error: Username or email address already exists</div>";
+                                    } else {
+                                        // Insert data into database
+                                        $sql = "INSERT INTO admin (username, password, admin_name, is_admin, is_super_admin, Email, PhoneNumber) 
+                                                VALUES ('$admin_username', '$admin_password', '$admin_name', '$is_admin', '$is_super_admin', '$admin_email', '$admin_phone')";
+
+                                        if ($conn->query($sql) === TRUE) {
+                                            echo "<div class='alert alert-success'>New admin added successfully</div>";
+                                        } else {
+                                            echo "<div class='alert alert-danger'>Error: " . $sql . "<br>" . $conn->error . "</div>";
+                                        }
+                                    }
+
+                                    $conn->close();
+                                }
+                                ?>
+                                <form method="post" class="text-left form-validate">
+                                    <div class="form-group-material">
+                                        <input id="register-username" type="text" name="registerUsername" required data-msg="Please enter your username" class="input-material">
+                                        <label for="register-username" class="label-material">Username</label>
+                                    </div>
+                                    <div class="form-group-material">
+                                        <input id="register-email" type="email" name="registerEmail" required data-msg="Please enter a valid email address" class="input-material">
+                                        <label for="register-email" class="label-material">Email Address</label>
+                                    </div>
+                                    <div class="form-group-material">
+                                        <input id="register-password" type="password" name="registerPassword" required data-msg="Please enter your password" class="input-material">
+                                        <label for="register-password" class="label-material">Password</label>
+                                    </div>
+                                    <div class="form-group-material">
+                                        <input id="register-name" type="text" name="registerName" required data-msg="Please enter the admin name" class="input-material">
+                                        <label for="register-name" class="label-material">Admin Name</label>
+                                    </div>
+                                    <div class="form-group-material">
+                                        <input id="register-phone" type="text" name="registerPhone" required data-msg="Please enter the phone number" class="input-material">
+                                        <label for="register-phone" class="label-material">Phone Number</label>
+                                    </div>
+                                    <div class="form-group-material">
+                                        <input id="is-admin" type="checkbox" name="isAdmin" class="input-material">
+                                        <label for="is-admin" class="label-material">Is Admin</label>
+                                    </div>
+                                    <div class="form-group-material">
+                                        <input id="is-super-admin" type="checkbox" name="isSuperAdmin" class="input-material">
+                                        <label for="is-super-admin" class="label-material">Is Super Admin</label>
+                                    </div>
+                                    <div class="form-group text-center">
+                                        <input id="register" type="submit" value="Register" class="btn btn-primary">
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-            <!-- Form Panel    -->
-            <div class="col-lg-6 bg-white">
-              <div class="form d-flex align-items-center">
-                <div class="content">
-                  <form class="text-left form-validate">
-                    <div class="form-group-material">
-                      <input id="register-username" type="text" name="registerUsername" required data-msg="Please enter your username" class="input-material">
-                      <label for="register-username" class="label-material">Username</label>
-                    </div>
-                    <div class="form-group-material">
-                      <input id="register-email" type="email" name="registerEmail" required data-msg="Please enter a valid email address" class="input-material">
-                      <label for="register-email" class="label-material">Email Address      </label>
-                    </div>
-                    <div class="form-group-material">
-                      <input id="register-password" type="password" name="registerPassword" required data-msg="Please enter your password" class="input-material">
-                      <label for="register-password" class="label-material">Password        </label>
-                    </div>
-                    <div class="form-group terms-conditions text-center">
-                      <input id="register-agree" name="registerAgree" type="checkbox" required value="1" data-msg="Your agreement is required" class="checkbox-template">
-                      <label for="register-agree">I agree with the terms and policy</label>
-                    </div>
-                    <div class="form-group text-center">
-                      <input id="register" type="submit" value="Register" class="btn btn-primary">
-                    </div>
-                  </form><small>Already have an account? </small><a href="login.html" class="signup">Login</a>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
-      <div class="copyrights text-center">
-         <p >2018 &copy; Your company. Download From <a target="_blank" href="https://templateshub.net">Templates Hub</a></p>
-      </div>
+        <div class="copyrights text-center">
+            <p>2024 &copy; CineTime.</p>
+        </div>
     </div>
     <!-- JavaScript files-->
     <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/popper.js/umd/popper.min.js"> </script>
+    <script src="vendor/popper.js/umd/popper.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-    <script src="vendor/jquery.cookie/jquery.cookie.js"> </script>
+    <script src="vendor/jquery.cookie/jquery.cookie.js"></script>
     <script src="vendor/chart.js/Chart.min.js"></script>
     <script src="vendor/jquery-validation/jquery.validate.min.js"></script>
     <script src="js/front.js"></script>
-  </body>
+</body>
 </html>
